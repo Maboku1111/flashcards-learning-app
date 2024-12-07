30a99a77535bf5e92cb5c0a7dfc0a6a0f95e9828
EzrahCodes <112862267+Maboku1111@users.noreply.github.com>
2024-12-07 16:26:41 +0200

    I have modified my services to consist of all of my necessary functions

--- client/src/app/services/flashcard.service.ts
+++ client/src/app/services/flashcard.service.ts
@@ -1,9 +1,33 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { FlashcardI } from '../types/flashcard.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FlashcardService {
+  private apiUrl = "http://localhost:3000"
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getAllFlashcards(): Observable<FlashcardI[]> {
+    return this.http.get<FlashcardI[]>(`${this.apiUrl}/flashcards`);
+  }
+
+  createFlashcard(question: string, answer: string, box: number): Observable<FlashcardI[]> {
+    return this.http.post<FlashcardI[]>(`${this.apiUrl}/flashcards`);
+  }
+
+  updateFlashcard(id: number, question: string, answer: string, box: number): Observable<FlashcardI[]> {
+    return this.http.put<FlashcardI[]>(`${this.apiUrl}/flashcards/${id}`)
+  }
+
+  deleteFlashcard(id: number): Observable<FlashcardI[]> {
+    return this.http.delete<FlashcardI[]>(`${this.apiUrl}/flashcards/${id}`)
+  }
+
+  moveFlashcard(id: number, box: number): Observable<FlashcardI[]> {
+    return this.http.patch<FlashcardI[]>(`${this.apiUrl}/flashcards/${id}/move`)
+  }
 }
