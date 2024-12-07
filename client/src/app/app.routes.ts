afe00d03debcbd0724302abb7f47bb07a3f3b1d8
EzrahCodes <112862267+Maboku1111@users.noreply.github.com>
2024-12-07 16:07:04 +0200

    Routing has been configured

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { OverviewComponent } from './ui/overview/overview.component';
+import { ReviewComponent } from './ui/review/review.component';
+import { NotFoundComponent } from './ui/not-found/not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '/', component: OverviewComponent, pathMatch: 'full' },
+  { path: 'review/:box', component: ReviewComponent, pathMatch: 'full' },
+  { path: '404', component: NotFoundComponent, pathMatch: 'full' },
+];
