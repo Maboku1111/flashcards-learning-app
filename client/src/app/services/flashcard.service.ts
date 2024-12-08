07c170e8e3e64f04bdf8ea389cbe3dee883059b4
EzrahCodes <112862267+Maboku1111@users.noreply.github.com>
2024-12-08 17:44:08 +0200

    Modified my services

--- client/src/app/services/flashcard.service.ts
+++ client/src/app/services/flashcard.service.ts
@@ -15,19 +15,33 @@ export class FlashcardService {
     return this.http.get<FlashcardI[]>(`${this.apiUrl}/flashcards`);
   }
 
-  createFlashcard(question: string, answer: string, box: number): Observable<FlashcardI[]> {
-    return this.http.post<FlashcardI[]>(`${this.apiUrl}/flashcards`);
+  createFlashcard(question: string, answer: string, box: number): Observable<FlashcardI> {
+    const flashcard = {
+      question,
+      answer,
+      box
+    }
+    return this.http.post<FlashcardI>(`${this.apiUrl}/flashcards`, flashcard);
   }
 
-  updateFlashcard(id: number, question: string, answer: string, box: number): Observable<FlashcardI[]> {
-    return this.http.put<FlashcardI[]>(`${this.apiUrl}/flashcards/${id}`)
+  updateFlashcard(id: number, question: string, answer: string, box: number): Observable<FlashcardI> {
+    const flashcard = {
+      question,
+      answer,
+      box
+    }
+    return this.http.put<FlashcardI>(`${this.apiUrl}/flashcards/${id}`, flashcard)
   }
 
-  deleteFlashcard(id: number): Observable<FlashcardI[]> {
-    return this.http.delete<FlashcardI[]>(`${this.apiUrl}/flashcards/${id}`)
+  deleteFlashcard(id: number): Observable<{}> {
+    return this.http.delete(`${this.apiUrl}/flashcards/${id}`)
   }
 
-  moveFlashcard(id: number, box: number): Observable<FlashcardI[]> {
-    return this.http.patch<FlashcardI[]>(`${this.apiUrl}/flashcards/${id}/move`)
+  moveFlashcard(id: number, box: number): Observable<FlashcardI> {
+    const flashcard = {
+      id,
+      box
+    }
+    return this.http.patch<FlashcardI>(`${this.apiUrl}/flashcards/${id}/move`, flashcard)
   }
 }
